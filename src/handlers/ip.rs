90bebd514f5be96c708b894fe837c3c6730aae67
ilmannafian04 <milmannafian04@gmail.com>
2023-07-21 21:03:40 +0700

    feat: check forwarded headers as ip

--- src/handlers/ip.rs
+++ src/handlers/ip.rs
@@ -3,7 +3,13 @@ use actix_web::{web, HttpRequest, HttpResponse, Responder};
 use crate::{services, states::IpCounter};
 
 pub async fn count_ip(ip_counter: web::Data<IpCounter>, req: HttpRequest) -> impl Responder {
-    let request_ip = &req.peer_addr().unwrap().ip().to_string();
+    let request_ip = match req.connection_info().realip_remote_addr() {
+        Some(ip) => ip.to_string(),
+        None => match req.peer_addr() {
+            Some(ip) => ip.to_string(),
+            None => return HttpResponse::BadRequest().finish(),
+        },
+    };
 
     let mut ip_map = match ip_counter.maps.lock() {
         Ok(lock) => lock,
